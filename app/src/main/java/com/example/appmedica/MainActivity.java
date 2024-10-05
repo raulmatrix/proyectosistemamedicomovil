09faaa241b7abd97d60be04d1d11f010bc2e2e5a
rvera <raulvp9@gmail.com>
2024-10-05 06:31:44 -0400

    adicion inicial de archivos, login completo con BD

---
+++ app/src/main/java/com/example/appmedica/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.appmedica;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText usuario, passw;
+    Button btnIng;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //getActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //getSupportActionBar().hide();
+
+        usuario = findViewById(R.id.edtUsuario);
+        passw = findViewById(R.id.edtPassword);
+        btnIng = findViewById(R.id.btnLogin);
+
+        btnIng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarLogin("http://192.168.218.236:9191/sistemamedico/remoto_validalogin.php");
+            }
+        });
+
+    }
+
+    private void validarLogin(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+
+                    if (success) {
+                        // Autenticación exitosa
+                        Intent intent = new Intent(getApplicationContext(), Principal.class);
+                        startActivity(intent);
+                    } else {
+                        // Autenticación fallida
+                        Toast.makeText(MainActivity.this, "Usuario o Contraseña Incorrectos", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Error en la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("usuario", usuario.getText().toString());
+                parametros.put("password", passw.getText().toString());
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+}
